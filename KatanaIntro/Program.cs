841ca153866f90bae7f6222afa56cb527e12e20e
Ken Wilcox <wilcoxk@medamerica.com>
2015-05-10 00:31:26 -0600

    Simple version works

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
 
 namespace KatanaIntro
 {
@@ -10,6 +12,25 @@ namespace KatanaIntro
     {
         static void Main(string[] args)
         {
+            var uri = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Starting...");
+                Console.ReadKey();
+                Console.WriteLine("Stopping...");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx =>
+            {
+                return ctx.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
